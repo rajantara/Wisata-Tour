b2e033a0dca074b3a96b46e717acf345ee59a77f
rajantara <rajanpensas@gmail.com>
2022-07-08 14:43:12 +0800

    update logout

--- logout.php
+++ logout.php
@@ -1,4 +1,13 @@
 <?php
+// Initialize the session
 session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session.
 session_destroy();
-header("location:login.php");
+
+// Redirect to login page
+header("location: login.php");
+exit;
